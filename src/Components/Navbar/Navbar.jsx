db3509941afbf81ce0581980076aecf13afb4c10
linhkhanh98 <linhkhnguyen.98@gmail.com>
2025-12-21 23:38:56 -0800

    dark-mode logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,16 @@ import { NavLink } from 'react-router-dom';
 import { MdLightMode } from "react-icons/md";
 import { FaMoon } from "react-icons/fa";
 import initialIcon from '../assets/logo-2.svg';
+import darkModeIcon from '../assets/logo-darkMode.svg';
 // import resumePDF from '../src/Components/assets/Main-Resume.pdf';
 
 const Navbar = ({isDarkMode, setIsDarkMode}) => {
     return (
         <nav className='navbar'>
             <NavLink to="/">
-                <img src={initialIcon} alt="Initial Icon" className='initial-logo'/>
+                <img src={isDarkMode ? darkModeIcon : initialIcon} alt="Initial Icon" className='initial-logo'/>
             </NavLink>
+
             
             <ul>
                 <li><NavLink to="/about">About</NavLink></li>
